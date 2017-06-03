7a8dabb6429396014109c93fcb78f753f51471d6
James <patchouly98@gmail.com>
2017-06-04 00:17:08 +0800

    section-1 finished

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,21 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+	var collection = new Array;
+	for (var count = 0;count<collectionA.length;count++) {
+		var temp = collectionA[count];
+		if(judgement(collectionB[0],temp)){
+			collection.push(temp);		
+		}			
+	}
+	return collection;
+}
+
+function judgement(array,x) {
+	for (var i = 0;i < array.length;i++) {
+		if (array[i] === x) {
+			return true;		
+		}
+	}
+	return false;
 }
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,21 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var collection = new Array;
+	for (var count = 0;count<collectionA.length;count++) {
+		var temp = collectionA[count].key;
+		if(judgement(objectB.value,temp)){
+			collection.push(temp);		
+		}			
+	}
+	return collection;
+}
+
+function judgement(array,x) {
+	for (var i = 0;i < array.length;i++) {
+		if (array[i] === x) {
+			return true;		
+		}
+	}
+	return false;
 }
