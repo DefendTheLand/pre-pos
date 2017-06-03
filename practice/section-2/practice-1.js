bc31ff5dc3f21786c748f4ef2bb9ff08481bc0a9
James <patchouly98@gmail.com>
2017-06-04 01:36:47 +0800

    finished

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let statistics = new Array;
+  for (var i = 0;i<collection.length;count++) {
+		 let temp = collection[i];
+		 if (judgement(statistics,temp)!=-1) {
+		 	statistics[i].count++;					 
+		 }else {
+		 	statistics.push({key:temp,count:1});		 
+		 }
+  }
+}
+
+function judgement(array,x) {
+	for (var i = 0;i < array.length;i++) {
+		if (array[i] === x) {
+			return i;		
+		}
+	}
+	return -1;
 }
--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,32 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let statistics = new Array;
+  for (var i = 0;i<collection.length;count++) {
+		 let temp = collection[i];
+		 if (collection[i].length<2) {
+			if (judgement(statistics,temp)!=-1) {
+		 		statistics[i].count++;					 
+		 	}else {
+		 		statistics.push({key:temp,count:1});		 
+		 	}		 
+		 }else {
+			var count = a.replace(/[^0-9]+/ig,"");
+			var word = a.replace(/[^a-z]+/ig,""); 
+			if (judgement(statistics,word)!=-1) {
+		 		statistics[i].count += count;					 
+		 	}else {
+		 		statistics.push({key:word,count:count});		 
+		 	}
+		 }
+  }
+}
+
+function judgement(array,x) {
+	for (var i = 0;i < array.length;i++) {
+		if (array[i] === x) {
+			return i;		
+		}
+	}
+	return -1;
 }
